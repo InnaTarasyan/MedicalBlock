722eaed9eb4575d2c6eab18cfef9756c0334432d
Inna Tarasyan <innatarasyanmail@gmail.com>
2026-01-15 15:45:33 +0400

    design fix

---
+++ app/Models/Doctor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+	use HasFactory;
+
+	protected $fillable = [
+		'npi',
+		'name',
+		'gender',
+		'city',
+		'state',
+		'taxonomy',
+		'organization_name',
+	];
+
+	/**
+	 * Get the blog posts authored by this doctor.
+	 */
+	public function blogPosts()
+	{
+		return $this->hasMany(BlogPost::class)->whereNotNull('published_at')->where('published_at', '<=', now())->latest('published_at');
+	}
+}
+
